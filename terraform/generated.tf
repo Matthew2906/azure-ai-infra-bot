12ca58783d4d819a355ab26584206b39515b5aa7
Matthew2906 <rmatthew457@gmail.com>
2026-03-19 22:58:43 -0400

    Changed Storage acc name

--- terraform/generated.tf
+++ terraform/generated.tf
@@ -1,12 +1,12 @@
-resource "azurerm_resource_group" "test_rg" {
+resource "azurerm_resource_group" "example" {
   name     = "test-rg"
   location = "East US"
 }
 
-resource "azurerm_storage_account" "teststore" {
-  name                     = "teststore"
-  resource_group_name      = azurerm_resource_group.test_rg.name
-  location                 = azurerm_resource_group.test_rg.location
-  account_tier            = "Standard"
+resource "azurerm_storage_account" "example" {
+  name                     = "azbotstore2906"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
   account_replication_type = "LRS"
 }
